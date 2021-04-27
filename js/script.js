06eeb1f897d54dd2c2094e6bc44d8b928a3d0496
bosciu <bosciu@icloud.com>
2021-04-27 15:56:11 +0200

    Completamento esercizio con aggiunta form

--- js/script.js
+++ js/script.js
@@ -4,15 +4,18 @@ il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca. */
-
+var inputKm = document.getElementById("input-km");
+var inputEta = document.getElementById("input-eta");
+var outputPrice = document.getElementById("root");
 
 function calcoloBiglietto() {
     /* Richiesta numero km & età del passeggiero */
-    var numeroKm = parseFloat(prompt("Quanti KM devi percorrere?"));
-    var etaPasseggero = parseInt(prompt("Quanti anni ha il passeggiero?"));
+    var numeroKm = Number(inputKm.value);
+    var etaPasseggero = Number(inputEta.value);
+    var prezzo = 0.21;
 
     /* Calcolo prezzo del biglietto */
-    var prezzoBiglietto = (numeroKm * 0.21).toFixed(2);
+    var prezzoBiglietto = (numeroKm * prezzo).toFixed(2);
 
     /* Sconto*/
     if (etaPasseggero < 18) {
@@ -20,4 +23,6 @@ function calcoloBiglietto() {
     } else if (etaPasseggero > 65) {
         var prezzoBiglietto = (prezzoBiglietto - (prezzoBiglietto * (40 / 100))).toFixed(2);
     }
+
+    outputPrice.innerHTML = "Il prezzo totale è di € " + prezzoBiglietto;
 }
