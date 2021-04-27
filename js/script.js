fb3b62c7fe44cf9a1a74a5221bb4be387c5e16e3
bosciu <bosciu@icloud.com>
2021-04-27 15:11:27 +0200

    Completamento Header

--- js/script.js
+++ js/script.js
@@ -5,16 +5,19 @@ va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca. */
 
-/* Richiesta numero km & età del passeggiero */
-var numeroKm = parseFloat(prompt("Quanti KM devi percorrere?"));
-var etaPasseggero = parseInt(prompt("Quanti anni ha il passeggiero?"));
 
-/* Calcolo prezzo del biglietto */
-var prezzoBiglietto = (numeroKm * 0.21).toFixed(2);
+function calcoloBiglietto() {
+    /* Richiesta numero km & età del passeggiero */
+    var numeroKm = parseFloat(prompt("Quanti KM devi percorrere?"));
+    var etaPasseggero = parseInt(prompt("Quanti anni ha il passeggiero?"));
 
-/* Sconto*/
-if (etaPasseggero < 18) {
-    var prezzoBiglietto = (prezzoBiglietto - (prezzoBiglietto * (20 / 100))).toFixed(2);
-} else if (etaPasseggero > 65) {
-    var prezzoBiglietto = (prezzoBiglietto - (prezzoBiglietto * (40 / 100))).toFixed(2);
+    /* Calcolo prezzo del biglietto */
+    var prezzoBiglietto = (numeroKm * 0.21).toFixed(2);
+
+    /* Sconto*/
+    if (etaPasseggero < 18) {
+        var prezzoBiglietto = (prezzoBiglietto - (prezzoBiglietto * (20 / 100))).toFixed(2);
+    } else if (etaPasseggero > 65) {
+        var prezzoBiglietto = (prezzoBiglietto - (prezzoBiglietto * (40 / 100))).toFixed(2);
+    }
 }
