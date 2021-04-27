26d8aeb8629a52a0943de9621463fef37640ae70
bosciu <bosciu@icloud.com>
2021-04-27 14:25:50 +0200

    Completamento parte logica

--- js/script.js
+++ js/script.js
@@ -6,15 +6,15 @@ va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca. */
 
 /* Richiesta numero km & età del passeggiero */
-var numeroKm = (parseFloat(prompt("Quanti KM devi percorrere?")));
+var numeroKm = parseFloat(prompt("Quanti KM devi percorrere?"));
 var etaPasseggero = parseInt(prompt("Quanti anni ha il passeggiero?"));
-console.log(numeroKm, etaPasseggero);
 
 /* Calcolo prezzo del biglietto */
 var prezzoBiglietto = (numeroKm * 0.21).toFixed(2);
-console.log(prezzoBiglietto);
 
-
-/* /* Formula per sconto
-var prezzoBigliettoScontato = (prezzoBiglietto - (prezzoBiglietto * (20 / 100))).toFixed(2);
-console.log(prezzoBigliettoScontato); */
+/* Sconto*/
+if (etaPasseggero < 18) {
+    var prezzoBiglietto = (prezzoBiglietto - (prezzoBiglietto * (20 / 100))).toFixed(2);
+} else if (etaPasseggero > 65) {
+    var prezzoBiglietto = (prezzoBiglietto - (prezzoBiglietto * (40 / 100))).toFixed(2);
+}
